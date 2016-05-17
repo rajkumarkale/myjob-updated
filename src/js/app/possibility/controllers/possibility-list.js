0061f464cb13273b2c07316677373b5d3ff16494
Revathi Bandi <rbandi@corp.paradigmit.com>
2016-05-17 13:05:41 +0530

    modifications in possibility view

--- src/js/app/possibility/controllers/possibility-list.js
+++ src/js/app/possibility/controllers/possibility-list.js
@@ -1,13 +1,7 @@
 angular.module('com.module.possibility')
 .controller('possibilityListController',['$scope','$state','possibilityService',function($scope,$state,possibilityService){
-
-	$scope.openCreatePossibility = function(){
-				$state.go('app.createPossibility');
-		}
 		possibilityService.getPossibilitiess().then(function(response){
-
 			console.log(response);
-
 		});
 
   }]);

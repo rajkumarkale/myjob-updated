cc5d96d132234ceafaae9e7cf88916704157b8f1
rajkumarkale <mr.rajkale@gmail.com>
2016-05-25 17:24:20 +0530

    added UI angular modal property to add discussions

--- src/js/app/prospect/controllers/view-discussionsCtrl.js
+++ src/js/app/prospect/controllers/view-discussionsCtrl.js
@@ -2,13 +2,18 @@
  * Created by rkale on 5/20/2016.
  */
 angular.module('com.module.prospect')
-.controller('viewDiscussionCtrl',['$scope',function($scope){
-  $scope.displayPopUp=function(id,ptr){
-    document.getElementById(id).style.display=ptr;
+.controller('viewDiscussionCtrl',['$scope','$modal',function($scope,$modal){
+  console.log('sample');
+  $scope.openAddDiscussions = function () {
+    var modalInstance = $modal.open({
+      templateUrl: 'js/app/prospect/views/add-new-discussions.html',
+      backdrop: 'static',
+      controller: 'ModalInstanceCtrl',
+      size: 'lg'
+    });
+    modalInstance.result.then(function () {
+    });
+  };
 
-  }
-    $scope.closeModal=function(id,ptr)
-    {
-      document.getElementById(id).style.display=ptr;
-    }
-}])
+}]);
+

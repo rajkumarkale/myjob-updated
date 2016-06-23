d31c8f53122b29563514c5c904854c4eae3340d1
rajkumarkale <mr.rajkale@gmail.com>
2016-06-23 17:10:04 +0530

    aligned components

--- src/js/app/suspect/controllers/suspect-viewCtrl.js
+++ src/js/app/suspect/controllers/suspect-viewCtrl.js
@@ -4,21 +4,5 @@
 
 angular.module('com.module.suspect')
   .controller('suspectViewCtrl',['$scope',function($scope){
-    $scope.open = function($event,opened) {
-      $event.preventDefault();
-      $event.stopPropagation();
 
-      $scope.openCal=opened;
-
-      if($scope.openCal==='opened1')
-      {
-        $scope.opened1 = true;
-        $scope.opened2 = false;
-      }
-      else if($scope.openCal==='opened2')
-      {
-        $scope.opened2 = true;
-        $scope.opened1 = false;
-      }
-    };
   }]);

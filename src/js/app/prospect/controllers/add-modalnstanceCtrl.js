34f01a12af542237f44ab9a05fca6b09d62aefe3
naveen Reddy Nomula <nreddy@corp.paradigmit.com>
2016-06-24 10:05:57 +0530

    discussions api integration

--- src/js/app/prospect/controllers/add-modalnstanceCtrl.js
+++ src/js/app/prospect/controllers/add-modalnstanceCtrl.js
@@ -3,17 +3,34 @@
  */
 
 angular.module('com.module.prospect')
-  .controller('ModalInstanceCtrl',function ($scope, $modalInstance,appConfig)
-  {
-    $scope.status = appConfig.discussion.typeOfDiscussion;
-    $scope.ok = function ()
-    {
-      $modalInstance.close();
-    };
-
-    $scope.cancel = function ()
-    {
-      $modalInstance.dismiss();
-    };
+    .controller('ModalInstanceCtrl', function ($scope, $modalInstance, appConfig, possibilityCreateService, discussionService,$cookies,$state) {
+        $scope.status = appConfig.discussion.typeOfDiscussion;
+        $scope.data = discussionService.getData();
+        $scope.userId = JSON.parse($cookies.userData).userDetails._id;
+        $scope.discussion = {};
+        
+        $scope.ok = function (discussion) {
+            console.log(discussion);
+            var reqData = {
+            text: $scope.discussion.summary,
+            mode: $scope.status.selectedItem.key,
+            client_unit_id: $scope.data.client_unit_id,
+            client_status_id: $scope.data.client_status,
+            discussed_by: $scope.userId,
+                venue:$scope.discussion.venue,
+                type:$scope.discussion.calltype
+        };
+         $scope.discussionPromise= possibilityCreateService.createDiscussion(reqData).success(function (response) {
+                /*toaster.pop('Success', 'Created Client successfully.');*/
+                console.log(response);
+                $modalInstance.close();
+                $state.reload();
+            }).error(function (err) {
+                $scope.authError = err.message;
+                toaster.pop('Fail', 'Failed to Create Client.');
+            });
+        };
+        $scope.cancel = function () {
+            $modalInstance.dismiss();
+        };
     });
-

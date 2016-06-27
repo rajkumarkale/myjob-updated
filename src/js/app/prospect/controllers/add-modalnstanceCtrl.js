d1316138365afa7df3d092e0d25b37b593cec623
naveen Reddy Nomula <nreddy@corp.paradigmit.com>
2016-06-27 05:41:16 +0530

    file upload logic

--- src/js/app/prospect/controllers/add-modalnstanceCtrl.js
+++ src/js/app/prospect/controllers/add-modalnstanceCtrl.js
@@ -3,11 +3,47 @@
  */
 
 angular.module('com.module.prospect')
-    .controller('ModalInstanceCtrl', function ($scope, $modalInstance, appConfig, possibilityCreateService, discussionService,$cookies,$state) {
+    .controller('ModalInstanceCtrl', function ($scope, $modalInstance, appConfig, possibilityCreateService, discussionService,$cookies,$state,Upload) {
         $scope.status = appConfig.discussion.typeOfDiscussion;
         $scope.data = discussionService.getData();
         $scope.userId = JSON.parse($cookies.userData).userDetails._id;
         $scope.discussion = {};
+    
+    $scope.uploadFiles=[];
+            $scope.$watch('files', function() {
+            $scope.upload($scope.files);
+        });
+            $scope.upload = function(files) {
+            if (files && files.length) {
+                for (var i = 0; i < files.length; i++) {
+                    var file = files[i];
+                    if (!file.$error) {
+                        Upload.upload({
+                            url: appConfig.apiUrl+'/api/upload/file',
+                            data: {
+                                content: file
+                            }
+                        }).then(function(resp) {
+                          file.url = resp.data.url;
+                          file.documentType = angular.copy(appConfig.possibility.documentType);
+                          $scope.uploadFiles.push(file);
+
+                          $scope.fileName=file.name;
+
+                          if (file.name.length > 7 ) {
+                          $scope.fileNamePart1 = file.name.substring(0, 8);
+                          $scope.fileNameLen = file.name.length - 7;
+                          $scope.fileNamePart2 = file.name.substring($scope.fileNameLen);
+                          $scope.fileName = $scope.fileNamePart1 + '...' + $scope.fileNamePart2
+                          console.log($scope.fileName+' '+file.name);
+                        }
+                        }, null, function(evt) {
+
+                        });
+                    }
+                }
+            }
+        };
         
         $scope.ok = function (discussion) {
             console.log(discussion);
@@ -20,15 +56,17 @@ angular.module('com.module.prospect')
                 venue:$scope.discussion.venue,
                 type:$scope.discussion.calltype
         };
-         $scope.discussionPromise= possibilityCreateService.createDiscussion(reqData).success(function (response) {
-                /*toaster.pop('Success', 'Created Client successfully.');*/
+            
+         /*$scope.discussionPromise= possibilityCreateService.createDiscussion(reqData).success(function (response) {
+                toaster.pop('Success', 'Created Client successfully.');
                 console.log(response);
                 $modalInstance.close();
                 $state.reload();
             }).error(function (err) {
                 $scope.authError = err.message;
                 toaster.pop('Fail', 'Failed to Create Client.');
-            });
+            });*/
+            $modalInstance.close();
         };
         $scope.cancel = function () {
             $modalInstance.dismiss();

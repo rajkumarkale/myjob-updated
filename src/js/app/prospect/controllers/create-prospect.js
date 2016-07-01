a1cd1af02b6e4e933a461e47cb7f3b959c32d705
kalyani-venna <kalyani.veena@mtuity.com>
2016-07-01 12:49:20 +0530

    made changes in create suspect,create prospect and update-profile UI

--- src/js/app/prospect/controllers/create-prospect.js
+++ src/js/app/prospect/controllers/create-prospect.js
@@ -1,9 +1,21 @@
 angular.module('com.module.prospect')
 .controller('prospectCreateController',['$scope','$state','appConfig',function($scope,$state,appConfig){
   $scope.values=appConfig.prospect.typeOfBusiness;
-  $scope.status=appConfig.prospect.status;
+  $scope.status_prospect=appConfig.prospect.status_prospect;
+  $scope.status=appConfig.suspect.status;
   $scope.displayagreement=false;
-  $scope.$watch('status.selectedItem',function(n,o){
+  $scope.Requirement=false;
+  $scope.requirement = function (close) {
+    $scope.titlereq='Add New Requirement';
+    if(close==='close')
+    {
+    $scope.Requirement=false;
+    }
+    else {
+    $scope.Requirement=true;
+    }
+  };
+  $scope.$watch('status_prospect.selectedItem',function(n,o){
     if(n.key==='Agreement_On_Closure'){
       $scope.title='Agreement on Closure';
       $scope.displayagreement=true;

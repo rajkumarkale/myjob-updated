86ebb76a9b4ad71ee4c8be27d540cb7e02653f2e
naveen Reddy Nomula <nreddy@corp.paradigmit.com>
2016-07-04 16:15:21 +0530

    dashboard datepicker

--- src/js/app/user/controllers/dashboard-controller.js
+++ src/js/app/user/controllers/dashboard-controller.js
@@ -49,7 +49,23 @@ angular.module('com.module.user').controller('dashboardController', ['$scope','A
     $scope.reflow = function () {
       $scope.$broadcast('highchartsng.reflow');
     };
+$scope.open = function($event,opened) {
+      $event.preventDefault();
+      $event.stopPropagation();
 
+      $scope.openCal=opened;
+
+      if($scope.openCal==='opened1')
+      {
+        $scope.opened1 = true;
+        $scope.opened2 = false;
+      }
+      else if($scope.openCal==='opened2')
+      {
+        $scope.opened2 = true;
+        $scope.opened1 = false;
+      }
+    };
 
 
 
